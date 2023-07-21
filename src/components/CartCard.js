58a3a7dd216dde49895863569b416f24fa8eb566
mechalr8 <harsitagrawal59@gmail.com>
2023-07-21 19:04:52 +0530

    first-commit

--- src/components/CartCard.js
+++ src/components/CartCard.js
@@ -2,7 +2,7 @@ import React from 'react'
 
 const CartCard = ({cartProduct}) => {
   return (
-    <div className='cart_card'>
+    <div className='cart-card'>
       <div>{cartProduct.name}</div>
       <div>{cartProduct.quantity} x {cartProduct.price}</div>
     </div>
